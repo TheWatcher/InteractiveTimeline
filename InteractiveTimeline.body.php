e1d8b7e0184121860fa46e18eb39ab51e3b39ec9
Chris <chris@starforge.co.uk>
2014-06-28 20:24:20 +0100

    More testing.

--- InteractiveTimeline.body.php
+++ InteractiveTimeline.body.php
@@ -8,9 +8,14 @@
 class InteractiveTimeline {
 
     public static function parserHook( $input, $args = array(), $parser, $frame ) {
-        $output = $parser -> recursiveTagParse( $input, $frame );
-        return '<pre>' . $output . '</pre>';
-    }
+        global $wgOut;
+        $options = array(
+            "width" => $args['width'] || 100,
+            "height" => $args['height'] || 200,
+        );
+
+        return Html::element('script', array('type' => 'text/javascript'), "var itdata_test = ".FormatJson::encode($options));
+   }
 
     public static function onParserFirstCallInit( &$parser ) {
         // Adds the <itimeline>...</itimeline> tag to the parser.
