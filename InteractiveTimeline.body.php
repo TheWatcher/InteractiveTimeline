e999b8bf1bea717e35aec73e359f1105f4ba75ff
Chris <chris@starforge.co.uk>
2014-07-04 23:41:03 +0100

    Oops, debugging.

--- InteractiveTimeline.body.php
+++ InteractiveTimeline.body.php
@@ -87,7 +87,7 @@ class InteractiveTimeline {
         //                   YYYY  - MM(restricted)- DD(restricted)           T ?  HH              :MM             :SS          Z +/-   HH            :MM
         if ( preg_match( "/^(\d{4})-?(0[1-9]|1[0-2])-?(0[1-9]|[12]\d|3[01])(?:[ T]([01]\d|2[0-4])(?::([0-5]\d))?(?::([0-5]\d))?([-+Z])(0\d|1[0-4])?(?::?([0-5]\d))?)?$/", $arg, $matches)){
             $valid = true;
-            print_r($matches);
+
             // Date must be specified in full
             $date = $matches[1] . "-" . $matches[2] . "-" . $matches[3] . "T";
 
