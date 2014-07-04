f71f06a5d8b6a93f18dcced5505145e688fe7429
Chris <chris@starforge.co.uk>
2014-07-04 15:56:57 +0100

    Draw timeline, maybe?

--- modules/ext.interactivetimeline.js
+++ modules/ext.interactivetimeline.js
@@ -14,8 +14,14 @@
         base.init = function() {
             base.config = JSON.parse( mw.config.get( base.$el.attr( 'id' ) ) );
 
-            //
+            // extract the data from the element
             base.data = buildData( base.$el );
+
+            // Build the new timeline
+            timeline = new links.Timeline(base.el, base.config);
+
+            // Draw our timeline with the created data and options
+            timeline.draw(base.data);
         };
 
         // Run initializer
@@ -24,12 +30,25 @@
 
     function buildData( container ) {
         var events = container.children('div.itl-event');
-        var data = { };
+        var data = [];
 
         events.each( function( index, elem ) {
-            var start = elem.children('div.itl-start')[0];
+                         var jElem = $(elem);
+                         var startdate = jElem.children('div.itl-start')[0];
+                         var enddate   = jElem.children('div.itl-end')[0];
+                         var body      = jElem.children('div.itl-body')[0];
 
-                         });
+                         var event = { 'start': new Date(startdate.innerText),
+                                       'content': body.innerHTML
+                                     };
+                         if(enddate) {
+                             event['end'] = new Date(enddate.innerText)
+                         }
+
+                         data.push(event);
+                     });
+
+        return data;
     };
 
 
