96ab81f9689a9994a7dcb0113320fd2a4d0b7743
Chris <chris@starforge.co.uk>
2016-07-23 12:30:49 +0100

    Add support for special page aliases

---
+++ InteractiveTimeline.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aliases for the InteractiveTimeline extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English */
+$aliases['en'] = array(
+	'InteractiveTimeline' => array( 'InteractiveTimeline' )
+);
